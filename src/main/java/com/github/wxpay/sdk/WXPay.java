81c12e7e1ca754b79d6ca13ed6a3b5405dfcdd14
xianlong217 <xianlong.liu@aliyun.com>
2019-09-21 13:35:57 +0800

    主干代码

--- src/main/java/com/github/wxpay/sdk/WXPay.java
+++ src/main/java/com/github/wxpay/sdk/WXPay.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WXPay {
-
+	// 我是master主干代码
     private WXPayConfig config;
     private SignType signType;
     private boolean autoReport;
